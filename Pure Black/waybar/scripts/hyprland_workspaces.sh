96ae109259d779f6216af0ac07b0cbac116fc09e
David <108707731+Jawuj@users.noreply.github.com>
2025-11-19 22:53:07 -0500

    updating dots

---
+++ Pure Black/waybar/scripts/hyprland_workspaces.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Listar workspaces activos
+hyprctl workspaces | awk '
+{
+    ws = $2
+    active = ($3 == "1") ? "*" : " "
+    printf "[%s%s] ", ws, active
+}
+END { print "" }'
+
